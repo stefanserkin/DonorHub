4ace572f23e6c6f807730621807f814189a2f373
Stefan Serkin <stefanserkin@gmail.com>
2023-09-02 20:30:33 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Donor Hub
+
+Donor Hub allows supporters to review and manage their commitments to the organization in Experience Cloud.
+
+Giving
+
+* Donate
+* Make payments on outstanding pledges
+* Review past donations
+* Download tax receipts
+
+Donor Engagement
+
+* Visualization of real-time progress towards goals
+* Stories of organizational impact
+
+## Docs
+
+- [Quip](https://quip.com/0TaaAFIYofXj/Donor-Hub)
---
+++ force-app/main/default/lwc/donorHubGiftHistory/donorHubGiftHistory.js
@@ -0,0 +1,82 @@
+import { LightningElement, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import getGiftHistory from '@salesforce/apex/DonorHubController.getGiftHistory';
+
+import USER_ID from '@salesforce/user/Id';
+import CONTACTID_FIELD from '@salesforce/schema/User.ContactId';
+import ACCOUNTID_FIELD from '@salesforce/schema/User.AccountId';
+
+const COLS = [
+    { label: 'Date', fieldName: 'closeDate', type: 'date',
+        typeAttributes:{
+            year: "numeric",
+            month: "long",
+            day: "2-digit"
+        }
+    },
+    { label: 'Amount', fieldName: 'amount', type: 'currency', 
+        typeAttributes: { 
+            currencyCode: 'USD'
+        },
+    },
+    {  
+        type: 'button',
+        typeAttributes: {
+            label: 'Download Receipt', 
+            name: 'Download Receipt', 
+            variant: 'brand-outline', 
+            iconName: 'utility:download', 
+            disabled: { fieldName: 'disableReceipt' },
+        }
+    }
+];
+
+export default class DonorHubGiftHistory extends LightningElement {
+    error;
+    isLoading = false;
+    cardTitle = 'My Gift History';
+    cardIconName = 'standard:opportunity';
+
+    cols = COLS;
+    userId = USER_ID;
+    accountId;
+    contactId;
+    wiredGiftHistory = [];
+    householdGifts;
+
+    @wire(getRecord, {
+		recordId: USER_ID,
+		fields: [CONTACTID_FIELD, ACCOUNTID_FIELD]
+	}) wireuser({
+		error,
+		data
+	}) {
+		if (error) {
+			this.error = error; 
+		} else if (data) {
+			this.contactId = data.fields.ContactId.value;
+			this.accountId = data.fields.AccountId.value;
+		}
+	}
+
+    @wire(getGiftHistory, {accountId: '$accountId'})
+    wiredResult(result) {
+        this.isLoading = true;
+        this.wiredGiftHistory = result;
+        if (result.data) {
+            this.householdGifts = result.data;
+            console.table(this.householdGifts);
+            this.error = undefined;
+        } else if (result.error) {
+            this.householdGifts = undefined;
+            this.error = result.error;
+            console.error(this.error);
+        }
+        this.isLoading = false;
+    }
+
+    handleDownloadReceipt(event) {
+        alert(`Here is the receipt for gift id ${event.detail.row.id}`);
+    }
+
+}
